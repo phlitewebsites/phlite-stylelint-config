23933baa6ed0405819933b88e6d201c999afee36
Dan Hayden <pythonic1@gmail.com>
2016-04-19 16:23:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'extends': 'stylelint-config-standard',
+  'rules': {
+    'font-family-name-quotes': 'single-where-recommended',
+    'function-url-quotes': 'single',
+    'string-quotes': 'single'
+  }
+};
